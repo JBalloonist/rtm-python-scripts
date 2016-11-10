b486dc84f9dce759037b99beec2d4fa937c05968
Rob Ward <wardrobert@gmail.com>
2016-11-10 19:25:59 +0000

    Updated tracking file to CSV and added pom_count function

--- RTM/pomodoro.py
+++ RTM/pomodoro.py
@@ -8,32 +8,46 @@ import csv, time, json, datetime
 now = datetime.datetime.now()
 todays_date = str(now.month) + '-' + str(now.day) + '-' + str(now.year)
 
-searches = ['list:work-today and status:incomplete',
+searches = ['custom search',
+            'list:work-today and status:incomplete',
             'list:work and due:tod and status:incomplete',
             'list:work and priority:1 and status:incomplete',
             'list:work and status:incomplete']
 
-for n, i in enumerate(searches, start=1):
+for n, i in enumerate(searches):
     print (str(n) + ': ' + i)
 print " "
 
-ind = int(raw_input('Which search would you like to run? Enter zero if you would like to enter your own search: '))
-for n, i in enumerate(searches, start=1):
-    if n == ind:
-        search = i
-        break
+ind = int(raw_input('Which search would you like to run?\nEnter zero if you would like to enter your own search: '))
+for n, i in enumerate(searches):
+    if ind > 0:
+        if n == ind:
+            search = i
+            break
     else:
         search = raw_input('Enter your search: ')
         break
+
+print 'Search ' + search
 
 run_search = 'yes'
 # search = 'list:work and (due:tod OR priority:1)'
 file_name = 'work'
 
-def all_pom(text):
+# sends detail of the task worked on to a master file for the month
+def all_pom(text_one, text_two):
+    tracking_list = list()
+    pom_list = list()
     now = datetime.datetime.now()
-    with open('/home/JBalloonist/RTM/pomodoro/tracking.txt', 'a') as out:
-        out.write(now.isoformat() + ' - ' + text +  '\n')
+    tracking_list.append(now.isoformat())
+    tracking_list.append(text_one)
+    tracking_list.append(text_two)
+    pom_list.append(tracking_list)
+
+    with open('/home/JBalloonist/RTM/pomodoro/' + now.strftime('%B') + '-tracking.csv', 'a') as f:
+        writer = csv.writer(f)
+        for i in pom_list:
+            writer.writerow(i)
 
 # function to get the time already spent on a task
 def add_time(time):
@@ -86,6 +100,7 @@ def parseJson(file):
                 add.append(n['task']['added']) # created date and time
                 add.append(n['task']['completed'])
                 add.append(n['task']['estimate'])
+                add.append(n['task']['due']) # due date
                 ids_list.append(add)
         if isinstance(i['taskseries'], dict):
             for n in i['taskseries']:
@@ -113,7 +128,6 @@ def parseJson(file):
 
 def timer(run):
     mins = 0
-    # total_mins = int(raw_input("How many minutes have you already worked on this task? "))
     if run == "start":
         # Loop until we reach 25 minutes running
         while mins != 25:
@@ -123,6 +137,7 @@ def timer(run):
             # Increment the minute total
             mins += 1
             # Bring up the dialog box here
+    print ">>>>>>>>>>>>>>>>>>>>>", mins
     return mins
 
 def user_select():
@@ -137,6 +152,7 @@ def user_select():
 
 def createApp(rtm):
     global run_search
+    print search
     if run_search == 'yes':
         rspTasks = rtm.tasks.getList(filter=search)
         with open(file_name + '.json', 'wb') as out:
@@ -148,24 +164,33 @@ def createApp(rtm):
     # timelines never expire
     #just hardcoding one instead of getting new one every time
     timelineNum = '1053897822'
-    now = datetime.datetime.now() + (datetime.timedelta(minutes=25))
-    due_time = now.isoformat()
+    now = datetime.datetime.now()
+    pom_end = (now + (datetime.timedelta(minutes=25))).isoformat()
+    break_end_time = (now + (datetime.timedelta(minutes=31))).isoformat()
     with open(file_name + '.csv') as csvfile:
         tasks = csv.reader(csvfile, delimiter=',')
         for n, i in enumerate(tasks, start=1):
             if task_num == n:
-                rtm.tasks.setDueDate(timeline= timelineNum, list_id= i[0], taskseries_id= i[1],
-                task_id= i[2], due=due_time, has_due_time='1', parse='0')
+                rtm.tasks.setDueDate(timeline= timelineNum, list_id= i[0],
+                taskseries_id= i[1], task_id= i[2], due=pom_end, has_due_time='1', parse='0')
                 minutes.append(i[6])
+                if add_time(minutes) == 0:
+                    rtm.tasksNotes.add(timeline= timelineNum, list_id= i[0],
+                    taskseries_id= i[1],task_id= i[2], note_title='Original Due Date', note_text=i[7])
                 print 'Existing time: ' + str(add_time(minutes) / 60) + ' hours ' + str(add_time(minutes) % 60) + ' minutes'
                 total_time = timer('start') + add_time(minutes)
                 print 'Total time: ' + str(total_time / 60) + ' hours ' + str(total_time % 60) + ' minutes'
-                # print 'Total time: ' + str(total_time)
+
                 rtm.tasks.setEstimate(timeline= timelineNum, list_id= i[0], taskseries_id= i[1],
                 task_id= i[2], estimate= str(total_time) + 'm')
+
                 rtm.tasksNotes.add(timeline= timelineNum, list_id= i[0], taskseries_id= i[1],
                 task_id= i[2], note_title= 'pomodoro ' + str(total_time/25), note_text= ' ')
-                all_pom(i[3] + ' - pomodoro ' + str(total_time/25))
+
+                all_pom(i[3], 'pomodoro ' + str(total_time/25))
+
+                rtm.tasks.setDueDate(timeline= timelineNum, list_id= i[0], taskseries_id= i[1],
+                task_id= i[2], due=break_end_time, has_due_time='1', parse='0')
 
 # creates RTM (the API keys and token)
 def test(apiKey, secret, token=None):
